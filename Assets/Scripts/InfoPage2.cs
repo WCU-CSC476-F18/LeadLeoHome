2a23e5a924d59b55c9f236bc2ff6dabd5c2355e8
Eric Long <el829072@wcupa.edu>
2018-11-30 17:04:52 -0500

    Finished implementing game information menus. User can now navigate through game information pages and return back to the start screen to begin playing.

---
+++ Assets/Scripts/InfoPage2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InfoPage2 : MonoBehaviour {
+
+    public Button retToTitleButton;
+
+	// Use this for initialization
+	void Start () {
+        Button returnToTitle = retToTitleButton.GetComponent<Button>();
+
+        returnToTitle.onClick.AddListener(retToTitle);
+	}
+	
+	void retToTitle()
+    {
+        SceneManager.LoadScene("_Start_Scene");
+    }
+}
