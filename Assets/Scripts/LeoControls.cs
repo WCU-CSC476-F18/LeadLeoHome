f46523b48bd5fdef9cded98820f05e66d9b922d8
Eric Long <el829072@wcupa.edu>
2018-12-07 17:18:57 -0500

    Updated the UI to function properly. Will work better for fields and highway levels when they are complete.

--- Assets/Scripts/LeoControls.cs
+++ Assets/Scripts/LeoControls.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LeoControls : MonoBehaviour {
+
+    //used to print the time
+    public Text runningTime, woodsTime, riverTime, fieldsTime, highwayTime;
+    float gameTimer = 0.0f;//used in Update
+    float levelTimer = 0.0f;
+    bool gameOver = false;
+    bool woodsDone = false;
+    bool riverDone = true;
+    bool fieldsDone = true;
+    bool highwayDone = true;
+    int s, m, woodsSec, woodsMin, riverSec, riverMin, fieldsSec, fieldsMin, highwaySec, highwayMin;
 
     //paw prints to leave the correct trail once found in woodslevel
     public GameObject pp1, pp2, pp3, pp4, pp5, pp6;
@@ -27,6 +39,55 @@ public class LeoControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+        //displaying timer
+        if (gameOver == false)
+        {
+            gameTimer += Time.deltaTime;
+
+            s = (int)(gameTimer % 60);
+            m = (int)(gameTimer / 60) % 60;
+
+            int sec = 59 - s;
+            int min = 14 - m;
+
+            string runTime = string.Format("{0:00}:{1:00}", m, s);//format the time nicely
+            string runTimeCountdown = string.Format("{0:00}:{1:00}", min, sec);//format the string to countdown
+
+            runningTime.text = ("Time until Animal Control arrives: " + runTimeCountdown);//set runTime to current run timer
+
+            //timer for the woods level
+            if (woodsDone == false)
+            {
+                woodsTime.text = ("The Woods: " + runTime);
+                woodsSec = s;
+                woodsMin = m;
+            }
+            //timer for the river level
+            if (riverDone == false)
+            {
+                levelTimer += Time.deltaTime;
+                riverSec = (int)(levelTimer % 60);
+                riverMin = (int)(levelTimer / 60) % 60;
+                riverTime.text = string.Format("The River: {0:00}:{1:00}", riverMin, riverSec);
+            }
+            //timer for the fields level
+            if (fieldsDone == false)
+            {
+                levelTimer += Time.deltaTime;
+                fieldsSec = (int)(levelTimer % 60);
+                fieldsMin = (int)(levelTimer / 60) % 60;
+                fieldsTime.text = string.Format("The Fields: {0:00}:{1:00}", fieldsMin, fieldsSec);
+            }
+            //timer for the highway level
+            if (highwayDone == false)
+            {
+                levelTimer += Time.deltaTime;
+                highwaySec = (int)(levelTimer % 60);
+                highwayMin = (int)(levelTimer / 60) % 60;
+                highwayTime.text = string.Format("The Highway: {0:00}:{1:00}", highwayMin, highwaySec);
+            }
+        }
         //get user input to make leo move
         float moveX = Input.GetAxis("Horizontal") * 15;
         float moveY = Input.GetAxis("Vertical") * 15;
@@ -92,6 +153,7 @@ public class LeoControls : MonoBehaviour {
             //if leo picked the right path--progress to next area
             if (other.gameObject.CompareTag("Finish"))
             {
+                Debug.Log("level" + level);
                 isTriggered = true;//set this to true so that the trigger isnt activated more than once
                 level++;
                 LevelHandler(level);
@@ -157,6 +219,7 @@ public class LeoControls : MonoBehaviour {
         }
     }
 
+    //need this to make sure leo doesnt teleport more than once when using triggers
     private void OnCollisionExit(Collision collision)
     {
         isTriggered = false;
@@ -172,55 +235,93 @@ public class LeoControls : MonoBehaviour {
                 rb.transform.position = new Vector3(0.0f, 0.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 break;
+
             case 2:
                 rb.transform.position = new Vector3(0.0f, 15.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 pp1.SetActive(true);//show paw prints for correct way found
                 break;
+
             case 3:
                 rb.transform.position = new Vector3(0.0f, 30.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 pp2.SetActive(true);//show paw prints for correct way found
                 break;
+
             case 4:
                 rb.transform.position = new Vector3(0.0f, 45.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 pp3.SetActive(true);//show paw prints for correct way found
                 break;
+
             case 5:
                 rb.transform.position = new Vector3(0.0f, 60.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 pp4.SetActive(true);//show paw prints for correct way found
                 break;
+
             case 6:
                 rb.transform.position = new Vector3(0.0f, 75.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 pp5.SetActive(true);//show paw prints for correct way found
                 break;
+
             case 7:
                 rb.transform.position = new Vector3(0.0f, 90.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 pp6.SetActive(true);//show paw prints for correct way found
+                isTriggered = false;//just in case
                 break;
+
             case 8:
                 //teleport to level 2 - the river
+
+                woodsDone = true;//stops the timer for the woods level
+                woodsTime.text = string.Format("The Woods: {0:00}:{1:00}", woodsMin, woodsSec);//set the score
+                riverDone = false;//start running the timer for the river
+
                 isTriggered = false;//since leo technically never stepped off the teleporting trigger
+
                 rb.transform.position = new Vector3(47.0f, 0.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 break;
+
             case 9:
                 //teleport to level 3 - the fields
+
+                riverDone = true;//stops the timer for the river level
+                riverTime.text = string.Format("The River: {0:00}:{1:00}", riverMin, riverSec);//set the score
+                levelTimer = 0.0f;//reset the level timer to 0 to be used for the fieldsTime
+                fieldsDone = false;//start running the timer for the fields
+
                 isTriggered = false;//since leo technically never stepped off the teleporting trigger
-                rb.transform.position = new Vector3(95.0f, 0.0f, -1.0f);
+
+                rb.transform.position = new Vector3(97.0f, 0.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 break;
+
             case 10:
                 //teleport to level 4 - the highway
+
+                fieldsDone = true;//stops the timer for the field level
+                fieldsTime.text = string.Format("The Fields: {0:00}:{1:00}", fieldsMin, fieldsSec);//set the score
+                levelTimer = 0.0f;//reset the level timer to 0 to be used for the highwayTime
+                highwayDone = false;//start running the timer for the fields
+
                 isTriggered = false;//since leo technically never stepped off the teleporting trigger
-                rb.transform.position = new Vector3(150.0f, 0.0f, -1.0f);
+
+                rb.transform.position = new Vector3(180.0f, 0.0f, -1.0f);
                 rb.velocity = Vector3.zero;//make sure they dont carry their momentum in the next room
                 break;
+
             case 11:
+                gameOver = true;//start the end-game scene
+
+                highwayDone = true;
+                highwayTime.text = string.Format("The Highway: {0:00}:{1:00}", highwayMin, highwaySec);
+
+                runningTime.text = string.Format("Congrats! You got Leo home in {0:00}:{1:00}!", m, s);
+
                 isTriggered = false;//since leo technically never stepped off the teleporting trigger
                 //game over -- load game over scene??
 
